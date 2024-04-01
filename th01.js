3486187876fd81bfbdf0ab07380f81a6bbb80f34
quandz12342 <165654123+quandz12342@users.noreply.github.com>
2024-04-01 18:47:15 +0700

    Add files via upload

---
+++ th01.js
@@ -0,0 +1,16 @@
+function updateClock() {
+    var currentTime = new Date();
+    var hours = currentTime.getHours();
+    var minutes = currentTime.getMinutes();
+    var seconds = currentTime.getSeconds();
+
+    hours = (hours < 10 ? "0" : "") + hours;
+    minutes = (minutes < 10 ? "0" : "") + minutes;
+    seconds = (seconds < 10 ? "0" : "") + seconds;
+
+    document.getElementById("hours").innerHTML = hours;
+    document.getElementById("minutes").innerHTML = minutes;
+    document.getElementById("seconds").innerHTML = seconds;
+}
+
+setInterval(updateClock, 1000);
